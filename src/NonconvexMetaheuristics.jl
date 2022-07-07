a854c8fcba70817245ad953ad9151b228a133989
Mohamed Tarek <mohamed82008@gmail.com>
2022-07-07 22:57:49 +1000

    first commit

--- src/NonconvexMetaheuristics.jl
+++ src/NonconvexMetaheuristics.jl
@@ -1,5 +1,107 @@
 module NonconvexMetaheuristics
 
-# Write your package code here.
+export MetaheuristicsAlg, MetaheuristicsOptions, Metaheuristics, ECA, DE, PSO, ABC, CGSA, SA, WOA, MCCGA, GA
+
+using Reexport, Parameters, Setfield, Random
+@reexport using NonconvexCore
+using NonconvexCore: @params, VecModel, AbstractResult
+using NonconvexCore: AbstractOptimizer, CountingFunction
+import NonconvexCore: optimize, optimize!, Workspace
+import Metaheuristics: Metaheuristics, ECA, DE, PSO, ABC, CGSA, SA, WOA, MCCGA, GA
+
+population_size(a::Metaheuristics.Algorithm) = a.parameters.N
+population_size(a::Metaheuristics.Algorithm{<:GA}) = a.parameters.selection.N
+
+struct MetaheuristicsAlg{A} <: AbstractOptimizer
+    algT::A
+end
+
+@params struct MetaheuristicsOptions
+    nt::NamedTuple
+end
+function MetaheuristicsOptions(; multiple_initial_solutions = false, N = 100, rng = Random.GLOBAL_RNG, kwargs...)
+    return MetaheuristicsOptions(merge(NamedTuple(kwargs), (; rng, N, multiple_initial_solutions)))
+end
+
+@params mutable struct MetaheuristicsWorkspace <: Workspace
+    model::VecModel
+    x0::AbstractVector
+    options::MetaheuristicsOptions
+    alg::MetaheuristicsAlg
+end
+function MetaheuristicsWorkspace(
+    model::VecModel, optimizer::MetaheuristicsAlg,
+    x0::AbstractVector = getinit(model);
+    options = MetaheuristicsOptions(), kwargs...,
+)
+    return MetaheuristicsWorkspace(model, copy(x0), options, optimizer)
+end
+@params struct MetaheuristicsResult <: AbstractResult
+    minimizer
+    minimum
+    result
+    alg
+    options
+end
+
+function optimize(model::NonconvexCore.Model, optimizer::MetaheuristicsAlg, x0::Vector, args...; options = MetaheuristicsOptions())
+    N = options.nt.N
+    @assert N > 0
+    if !(options.nt.multiple_initial_solutions)
+        x0 = [x0]
+    end
+    @assert eltype(x0) <: AbstractVector
+    first_x0 = identity.(first(x0))
+    flat_x0 = first.(NonconvexCore.flatten.(map(x -> identity.(x), x0)))
+    NonconvexCore._optimize_precheck(model, optimizer, first_x0)
+    _model, _, unflatten = NonconvexCore.tovecmodel(model, first_x0)
+    r = optimize(_model, optimizer, flat_x0; options)
+    return @set r.minimizer = unflatten(r.minimizer)
+end
+function optimize(model::NonconvexCore.DictModel, optimizer::MetaheuristicsAlg, x0, args...; options = MetaheuristicsOptions())
+    N = options.nt.N
+    @assert N > 0
+    if !(options.nt.multiple_initial_solutions)
+        x0 = [x0]
+    end
+    @assert typeof(x0) <: AbstractVector
+    @assert eltype(x0) <: AbstractDict
+    first_x0 = first(x0)
+    flat_x0 = first.(NonconvexCore.flatten.(x0))
+    NonconvexCore._optimize_precheck(model, optimizer, first_x0)
+    _model, _, unflatten = NonconvexCore.tovecmodel(model, first_x0)
+    r = optimize(_model, optimizer, flat_x0; options)
+    return @set r.minimizer = unflatten(r.minimizer)
+end
+
+@generated function drop_ks(nt::NamedTuple{names}, ::Val{ks}) where {names, ks}
+    ns = Tuple(setdiff(names, ks))
+    return :(NamedTuple{$ns}(nt))
+end
+
+function optimize!(workspace::MetaheuristicsWorkspace)
+    @unpack model, options, x0, alg = workspace
+    _alg = alg.algT(; drop_ks(options.nt, Val((:multiple_initial_solutions, :rng)))...)
+    N = options.nt.N
+    obj = NonconvexCore.getobjective(model)
+    ineq = NonconvexCore.getineqconstraints(model)
+    eq = NonconvexCore.geteqconstraints(model)
+    mh_func = x -> begin
+        obj(x), (length(ineq.fs) === 0 ? [0.0] : ineq(x)), (length(eq.fs) === 0 ? [0.0] : eq(x))
+    end
+    bounds = hcat(getmin(model), getmax(model))'
+    newx = [rand(options.nt.rng, length(first(x0))) .* (getmax(model) .- getmin(model)) .+ getmin(model) for _ in 1:N-length(x0)]
+    x0 = [x0; newx]
+    population = [Metaheuristics.create_child(x, mh_func(x)) for x in x0]
+    prev_status = Metaheuristics.State(Metaheuristics.get_best(population), population)
+    _alg.status = prev_status
+    result = Metaheuristics.optimize(mh_func, bounds, _alg)
+
+    return MetaheuristicsResult(Metaheuristics.minimizer(result), Metaheuristics.minimum(result), result, alg, options)
+end
+
+function Workspace(model::VecModel, optimizer::MetaheuristicsAlg, args...; kwargs...,)
+    return MetaheuristicsWorkspace(model, optimizer, args...; kwargs...)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,57 @@
-using NonconvexMetaheuristics
-using Test
+using NonconvexMetaheuristics, LinearAlgebra, Test, Random
 
-@testset "NonconvexMetaheuristics.jl" begin
-    # Write your tests here.
+f(x::AbstractVector) = 100sqrt(x[2])
+g(x::AbstractVector, a, b) = (a*x[1] + b)^3 - x[2]
+
+function get_initial(x0, multiple_initial_solutions, N)
+    if multiple_initial_solutions
+        return [[x0]; [rand(2) * 10 for _ in 1:N-1]]
+    else
+        return x0
+    end
+end
+
+@testset "Multiple initial solutions: $multiple_initial_solutions" for multiple_initial_solutions in (true, false)
+    options = MetaheuristicsOptions(; multiple_initial_solutions, N = 1000)
+    @testset "Simple constraints" begin
+        Random.seed!(1)
+        m = Model(f)
+        addvar!(m, [0.0, 0.0], [10.0, 10.0])
+        add_ineq_constraint!(m, x -> g(x, 2, 0))
+        add_ineq_constraint!(m, x -> g(x, -1, 1))
+
+        alg = MetaheuristicsAlg(ECA)
+        x0 = get_initial([1.234, 2.345], multiple_initial_solutions, 1000)
+        r = NonconvexCore.optimize(m, alg, x0, options = options)
+        @test abs(r.minimum - 100sqrt(8/27)) < 1e-2
+        @test norm(r.minimizer - [1/3, 8/27]) < 1e-2
+    end
+
+    @testset "Equality constraints" begin
+        Random.seed!(1)
+        m = Model(f)
+        addvar!(m, [0.0, 0.0], [10.0, 10.0])
+        add_ineq_constraint!(m, x -> g(x, 2, 0))
+        add_ineq_constraint!(m, x -> g(x, -1, 1))
+        add_eq_constraint!(m, x -> sum(x) - 1/3 - 8/27)
+
+        alg = MetaheuristicsAlg(ECA)
+        x0 = get_initial([1.234, 2.345], multiple_initial_solutions, 1000)
+        r = NonconvexCore.optimize(m, alg, x0, options = options)
+        @test abs(r.minimum - 100sqrt(8/27)) < 1e-2
+        @test norm(r.minimizer - [1/3, 8/27]) < 1e-2
+    end
+
+    @testset "Block constraints" begin
+        Random.seed!(1)
+        m = Model(f)
+        addvar!(m, [0.0, 0.0], [10.0, 10.0])
+        add_ineq_constraint!(m, FunctionWrapper(x -> [g(x, 2, 0), g(x, -1, 1)], 2))
+
+        alg = MetaheuristicsAlg(ECA)
+        x0 = get_initial([1.234, 2.345], multiple_initial_solutions, 1000)
+        r = NonconvexCore.optimize(m, alg, x0, options = options)
+        @test abs(r.minimum - 100sqrt(8/27)) < 1e-2
+        @test norm(r.minimizer - [1/3, 8/27]) < 1e-2
+    end
 end
