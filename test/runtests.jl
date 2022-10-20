658c74d261e2e39f3641e406d61e8f7b2cd44350
mohamed82008 <mohamed82008@gmail.com>
2022-10-21 01:04:28 +1100

    fix and test GA and lower bound MH

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using NonconvexMetaheuristics, LinearAlgebra, Test, Random
 f(x::AbstractVector) = 100sqrt(x[2])
 g(x::AbstractVector, a, b) = (a*x[1] + b)^3 - x[2]
 
+tol = 2e-2
 function get_initial(x0, multiple_initial_solutions, N)
     if multiple_initial_solutions
         return [[x0]; [rand(2) * 10 for _ in 1:N-1]]
@@ -11,7 +12,7 @@ function get_initial(x0, multiple_initial_solutions, N)
     end
 end
 
-@testset "Multiple initial solutions: $multiple_initial_solutions" for multiple_initial_solutions in (true, false)
+@testset "Multiple initial solutions: $multiple_initial_solutions, alg: $malg" for multiple_initial_solutions in (true, false), malg in (ECA, GA)
     options = MetaheuristicsOptions(; multiple_initial_solutions, N = 1000)
     @testset "Simple constraints" begin
         Random.seed!(1)
@@ -20,11 +21,11 @@ end
         add_ineq_constraint!(m, x -> g(x, 2, 0))
         add_ineq_constraint!(m, x -> g(x, -1, 1))
 
-        alg = MetaheuristicsAlg(ECA)
+        alg = MetaheuristicsAlg(malg)
         x0 = get_initial([1.234, 2.345], multiple_initial_solutions, 1000)
         r = NonconvexCore.optimize(m, alg, x0, options = options)
-        @test abs(r.minimum - 100sqrt(8/27)) < 1e-2
-        @test norm(r.minimizer - [1/3, 8/27]) < 1e-2
+        @test abs(r.minimum - 100sqrt(8/27)) < tol
+        @test norm(r.minimizer - [1/3, 8/27]) < tol
     end
 
     @testset "Equality constraints" begin
@@ -35,11 +36,11 @@ end
         add_ineq_constraint!(m, x -> g(x, -1, 1))
         add_eq_constraint!(m, x -> sum(x) - 1/3 - 8/27)
 
-        alg = MetaheuristicsAlg(ECA)
+        alg = MetaheuristicsAlg(malg)
         x0 = get_initial([1.234, 2.345], multiple_initial_solutions, 1000)
         r = NonconvexCore.optimize(m, alg, x0, options = options)
-        @test abs(r.minimum - 100sqrt(8/27)) < 1e-2
-        @test norm(r.minimizer - [1/3, 8/27]) < 1e-2
+        @test abs(r.minimum - 100sqrt(8/27)) < tol
+        @test norm(r.minimizer - [1/3, 8/27]) < tol
     end
 
     @testset "Block constraints" begin
@@ -48,10 +49,10 @@ end
         addvar!(m, [0.0, 0.0], [10.0, 10.0])
         add_ineq_constraint!(m, FunctionWrapper(x -> [g(x, 2, 0), g(x, -1, 1)], 2))
 
-        alg = MetaheuristicsAlg(ECA)
+        alg = MetaheuristicsAlg(malg)
         x0 = get_initial([1.234, 2.345], multiple_initial_solutions, 1000)
         r = NonconvexCore.optimize(m, alg, x0, options = options)
-        @test abs(r.minimum - 100sqrt(8/27)) < 1e-2
-        @test norm(r.minimizer - [1/3, 8/27]) < 1e-2
+        @test abs(r.minimum - 100sqrt(8/27)) < tol
+        @test norm(r.minimizer - [1/3, 8/27]) < tol
     end
 end
